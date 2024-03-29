731236f56d632b677141f90ac9b8ae2923ec41be
Riley Flynn <riley@rileyflynn.me>
2024-03-29 00:35:00 -0230

    WIP 3d nonsense

--- src/components/home/background/scene.tsx
+++ src/components/home/background/scene.tsx
@@ -1,37 +1,64 @@
-import { Canvas, useFrame, type ThreeElements } from '@react-three/fiber';
-import { useRef, useState } from 'react';
+import { CameraControls, Center, PerspectiveCamera, useHelper } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { getProject } from '@theatre/core';
+import { SheetProvider, editable as e } from '@theatre/r3f';
+import extension from '@theatre/r3f/dist/extension';
+import studio from '@theatre/studio';
+import { useRef } from 'react';
 import * as THREE from 'three';
+import { SpotLight, SpotLightHelper } from 'three';
 
-function Box(props: ThreeElements['mesh']) {
-    const meshRef = useRef<THREE.Mesh>(null!);
-    const [hovered, setHover] = useState(false);
-    const [active, setActive] = useState(false);
-    useFrame((state, delta) => (meshRef.current.rotation.x += delta));
+if (import.meta.env.DEV) {
+    studio.initialize();
+    studio.extend(extension);
+}
+
+const sheet = getProject('rileyflynn.me').sheet('home');
+
+function TestScene({
+    canvasRef,
+    cameraRef,
+}: {
+    canvasRef: React.RefObject<HTMLCanvasElement>;
+    cameraRef: React.RefObject<THREE.PerspectiveCamera>;
+}) {
+    const lightRef = useRef<SpotLight>();
+    useHelper(lightRef, SpotLightHelper);
+    const meshRef = useRef<THREE.Mesh>();
 
     return (
-        <mesh
-            {...props}
-            ref={meshRef}
-            scale={active ? 1.5 : 1}
-            onClick={(event) => setActive(!active)}
-            onPointerOver={(event) => setHover(true)}
-            onPointerOut={(event) => setHover(false)}
-        >
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-        </mesh>
+        <SheetProvider sheet={sheet}>
+            <e.spotLight
+                position={[5, 3, 0]}
+                scale={[1, 0.8, 1]}
+                theatreKey="test"
+                castShadow
+                ref={lightRef}
+                target={meshRef.current}
+                intensity={2}
+                penumbra={1}
+                angle={0.25}
+            />
+            <Center>
+                <mesh castShadow ref={meshRef}>
+                    <boxGeometry args={[1, 1, 1]} />
+                    <meshStandardMaterial color="orange" />
+                </mesh>
+            </Center>
+            <e.mesh theatreKey="floor" position={[0, -0.5, 0]} receiveShadow>
+                <boxGeometry args={[10, 0.1, 10]} />
+                <meshStandardMaterial color="grey" />
+            </e.mesh>
+        </SheetProvider>
     );
 }
 
 export function Scene() {
     return (
-        <Canvas>
-            <color attach="background" args={[0, 0, 0]} />
-            {/* <ambientLight intensity={Math.PI / 2} /> */}
-            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-            <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-            <Box position={[-1.2, 0, 0]} />
-            <Box position={[1.2, 0, 0]} />
+        <Canvas shadows>
+            <CameraControls makeDefault />
+            <PerspectiveCamera makeDefault position={[5, 10, 0]} />
+            <TestScene />
         </Canvas>
     );
 }
